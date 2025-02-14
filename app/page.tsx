1f159ad114a0754a78fe2ada2d51950a856af5b9
wangxk-office <wang_xukun@163.com>
2025-02-14 09:20:59 +0800

    initial app - setup prettier

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         远路漫漫的学习播客
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">footer</footer>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        footer
+      </footer>
     </div>
   );
 }
